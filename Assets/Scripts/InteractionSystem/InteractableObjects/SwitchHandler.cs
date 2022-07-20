64e0e75addad91ba494a2b5dd4ffc8177290ae2f
LegendOfZia <94755908+LegendOfZia@users.noreply.github.com>
2022-07-20 19:34:03 -0400

    Reworked Interactables
    Created a new class that can be easily subclassed in the form of InteractableObject. This has a customizable Toggle() function that can be used to create different interactions for different types of objects (eg. Doors, Switches).
    SwitchHandler may contain some unnecessary redundancy as it calls the Toggle() of the remote door. But ObjectIsTriggerable could possibly meet the same requirement. Would love a second set of eyes on that issue.

---
+++ Assets/Scripts/InteractionSystem/InteractableObjects/SwitchHandler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchHandler : InteractableObject
+{
+    public GameObject stick;
+    public List<GameObject> targetObjs;
+    private List<InteractableObject> targetInteracts;
+
+    public void Start()
+    {
+        targetInteracts = new List<InteractableObject>();
+        foreach (GameObject obj in targetObjs)
+        {
+            if (obj.GetComponent<InteractableObject>() != null)
+            {
+                targetInteracts.Add(obj.GetComponent<InteractableObject>());
+            }
+            else
+            {
+                print("Designated target object: " + obj.name + "does not contain an InteractableObject script");
+            }
+        }
+    }
+
+    public override void Toggle()
+    {
+        base.Toggle();
+        Vector3 tmpPos = stick.transform.localPosition;
+        if (_switched == true)
+        {
+            tmpPos.z = -0.08f;
+        }
+        else
+        {
+            tmpPos.z = 0.08f;
+        }
+        stick.transform.localPosition = tmpPos;
+        foreach(InteractableObject inter in targetInteracts)
+        {
+            inter.Toggle();
+        }
+    }
+}
